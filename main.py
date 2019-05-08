9ef711ef81bb09a4ea952183f9780c95bee2db56
Lornatang <shiyipaisizuo@163.com>
2019-05-08 11:12:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,31 @@
-from sklearn.linear_model import LinearRegression
-from ops import compute_grad_mbgd, update_beta, loss
+""" Main func.
+"""
 
-import pandas as pd
-import numpy as np
+####################################################
+# Author:  <Changyu Liu>shiyipaisizuo@gmail.com
+# License: MIT
+####################################################
 
-# input data
-train = pd.read_csv('data/train.csv')
-test = pd.read_csv('data/test.csv')
-submit = pd.read_csv('data/sample_submit.csv')
+from model import dnn
 
-# initial setup params
-beta = [1, 1]
-lr = 0.2
-tol_L = 0.1
-batch_size = 16
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
 
-# normalizes x
-max_x = max(train['id'])
-x = train['id'] / max_x
-y = train['questions']
+X_data, y_data = load_breast_cancer(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, train_size=0.6, test_size=0.4, random_state=28)
 
-# do the first calculation
-np.random.seed(10)
-grad = compute_grad_mbgd(beta, batch_size, x, y)
-losses = loss(beta, x, y)
-beta = update_beta(beta, lr, grad)
-new_losses = loss(beta, x, y)
+X_train = X_train.T
+y_train = y_train.reshape(y_train.shape[0], -1).T
 
-i = 1
-while np.abs(new_losses - losses) > tol_L:
-  beta = update_beta(beta, lr, grad)
-  grad = compute_grad_mbgd(beta, batch_size, x, y)
-  losses = new_losses
-  new_losses = loss(beta, x, y)
-  i += 1
-  print(f'Iter {i} SGD loss {abs(new_losses - losses):.6f}')
+X_test = X_test.T
+y_test = y_test.reshape(y_test.shape[0], -1).T
 
+accuracy = dnn(X_train,
+               y_train,
+               X_test,
+               y_test,
+               layer_dims=[X_train.shape[0], 10, 5, 1],
+               learning_rate=1e-3,
+               iters=10000)
 
-val = LinearRegression()
-val.fit(train[['id']], train[['questions']])
-
-print(f'Our     Coef: {beta[1] / max_x}')
-print(f'Sklearn Coef: {val.coef_[0][0]}')
-
-print(f'Our     Intercept: {beta[0]}')
-print(f'Sklearn Intercept: {val.intercept_[0]}')
-
-our_losses = loss(beta, x, y)
-sklearn_losses = loss([val.intercept_[0], val.coef_[0][0]], train['id'], y)
-print(f'Our     loss: {our_losses}')
-print(f"Sklearn loss: {sklearn_losses}")
+print(f"Acc: {accuracy}")
